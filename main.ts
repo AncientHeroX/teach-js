a1adc11edfffe77b5b8cf6a49a00a40d192931e9
Eduard Gonzalez <6415127+AncientHeroX@users.noreply.github.com>
2025-08-08 11:41:35 -1000

    Basic Serve
    Serve basic website

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
+import * as http from "@std/http";
+
 const PORT = 5000;
 
 function handler(request: Request): Response | Promise<Response> {
-  console.log(request);
-  return new Response("Hello, world");
+  const pathname = new URL(request.url).pathname;
+
+  console.log(pathname);
+  if (pathname === "/") {
+    return http.serveFile(request, "public/views/index.html");
+  }
+
+  if (pathname.startsWith("/static/css")) {
+    return http.serveDir(request, {
+      fsRoot: "public",
+      urlRoot: "static",
+    });
+  }
+
+  return new Response("404: Not Found", {
+    status: 302,
+    headers: { "Location": "/" },
+  });
 }
 
 Deno.serve({ port: PORT }, handler);
